13e89cc0f5ae47f8e260dba2d7f011c0381f7711
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2024-11-21 17:17:51 -0300

    Update servicos-cadastro.php

--- servicos-cadastro.php
+++ servicos-cadastro.php
@@ -1,2 +1,17 @@
-<?php
-// Formulário
+<!-- Formulário -->
+<?php include "header.php"; ?>
+
+<main>
+    <h2>Adicionar novo serviço</h2>
+    <form>
+        <label>Serviço: <input></label> <br>
+        <label>Descrição: <input></label> <br>
+        <label>Preço: <input></label> <br>
+        <label>Categoria: <input></label> <br>
+
+        <button>Salvar</button>
+    </form>
+
+</main>
+
+<?php include "footer.php" ?>
