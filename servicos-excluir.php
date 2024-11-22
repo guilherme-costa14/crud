e17e8935d94bc7f80e87e58ffe483d4f259d2fb1
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2024-11-22 16:36:47 -0300

    Update servicos-excluir.php

--- servicos-excluir.php
+++ servicos-excluir.php
@@ -1,12 +1,18 @@
 <?php
 // requisitar os dados
+$id = $_GET['id'];
 
 // montar um sql de delete
+$sql = "delete from servicos where id = $id";
 
 // incluir o arquivo de conexão
+include "conexao.php";
 
 // executar o sql delete no BD
+$resultado = mysqli_query($conexao, $sql);
 
 // fechar a conexão
+mysqli_close($conexao);
 
 // redirecionar para a página listar
+header("Location: servicos-listar.php");
