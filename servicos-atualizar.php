3d34cd8315b7a713111fd0974bd5cd4695c403f0
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2024-11-22 17:33:57 -0300

    Update servicos-atualizar.php

--- servicos-atualizar.php
+++ servicos-atualizar.php
@@ -1,22 +1,23 @@
 <?php
- // requisitar os dados do formulário
+// requisitar os dados do formulário
 $id = $_GET['id'];
 $servico = $_POST['servico'];
 $descricao = $_POST['descricao'];
 $preco = $_POST['preco'];
 $categoria = $_POST['categoria'];
 
- // montar um sql de update
- $sql = "update servicos set servico";
+// montar um sql de update
+ $sql = "update servicos set servico = 
+ '$servico', descricao = '$descricao', preco = '$preco', categoria = '$categoria' where id = $id";
 
- // incluir o arquivo de conexão
+// incluir o arquivo de conexão
+include "conexao.php";
 
+//executar o sql insert update no BD
+$resultado = mysqli_query($conexao, $sql);
 
- //executar o sql insert update no BD
+// fechar a conexão
+mysqli_close($conexao);
 
-
- // fechar a conexão
-
-
- // redirecionar para a página listar
- header("Location: servicos-listar.php");
+// redirecionar para a página listar
+header("Location: servicos-listar.php");
