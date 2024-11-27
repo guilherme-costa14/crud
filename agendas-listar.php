fc999a5a5f034f3ba79d5c0c8b59070ddc1ebade
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2024-11-27 16:45:12 -0300

    Create agendas-listar.php

---
+++ agendas-listar.php
@@ -0,0 +1,57 @@
+<?php 
+include "header.php"; 
+include "conexao.php"; 
+?>
+<main>
+
+<h2>Horário clientes</h2>
+<a href="agendas-cadastro.php"></a>
+
+<table border="2">
+    <tr>
+        <th>NOME</th>
+        <th>DATA</th>
+        <th>HORÁRIO</th>
+        <th>SERVIÇO</th>
+        <th>PROFISSIONAL</th>
+        <th>AÇÕES</th>
+    </tr>
+<?php
+
+$sql = "select * from clientes";
+$resultado = mysqli_query($conexao, $sql);
+
+while($linha = mysqli_fetch_assoc($resultado)){
+    echo "<tr>"; // interpolação de string
+    echo "<td> {$linha['nome']} </td>";
+    echo "<td> {$linha['bairro']} </td>";
+    echo "<td> {$linha['email']} </td>";
+    echo "<td> {$linha['contato']} </td>";
+
+    echo "<td>";
+    echo "<a href='clientes-excluir.php?id={$linha['id']}'>";
+    echo "<img src='delete.png'>";
+    echo "</a>";
+
+    echo " <a href='clientes-editar.php?id={$linha['id']}'>";
+    echo "<img src='edit.png'>";
+    echo "</a>";
+
+    echo "</td>";
+
+    echo "</tr>";
+}
+
+mysqli_close($conexao);
+
+/*
+// para debugar
+echo "<pre>";
+print_r($resultado);
+echo "</pre>";
+// fim debugar
+*/
+?>
+</table>
+</main>
+<?php include "footer.php"; ?>
